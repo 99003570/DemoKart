f23085aff8aa6f166186888c95e39629305d5fa4
99003668 <78849805+99003668@users.noreply.github.com>
2021-02-25 23:55:28 +0530

    Update User.cs

--- DemoKartBL/User.cs
+++ DemoKartBL/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace DemoKartBL
 {
-    class User
+    public class User
     {
+        public String UserName { get; set; }
+        public String Password { get; set; }
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public DateTime Dob { get; set; }
+        public long mobileNo { get; set; }
+
+        public User()
+        {
+
+        }
+        public User(String UserName,String Password,DateTime Dob,long mobileNo)
+        {
+            this.UserName = UserName;
+            this.Password = Password;
+            this.Dob = Dob;
+            this.mobileNo = mobileNo;
+        }
+        public int Login(String uName, String psword)
+        {
+            if (uName == UserName && psword == Password)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
     }
 }
